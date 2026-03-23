c3fa1a91b95262ff2747133a67016f56b75f00fa
LilBlackBiscuit <zolabs@pm.me>
2026-03-23 17:01:40 -0400

    Fix name conflict

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ REGION: str = os.getenv(key="AWS_REGION", default="us-east-1")
 
 
 app: aws_cdk.App = aws_cdk.App()
-sharkord_stack: SharkordStack = SharkordStack(scope=app, id="SharkordStack")
+sharkord_stack: SharkordStack = SharkordStack(scope=app, id="sharkord_stack")
 aws_cdk.Tags.of(scope=sharkord_stack).add(key="application", value="sharkord")
 app.synth(force=True, validate_on_synthesis=True)
