169c484e0727555b4d2bf6ee3f9c55dd4615b8cd
LilBlackBiscuit <zolabs@pm.me>
2026-03-23 17:15:31 -0400

    Update stack environment

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ import aws_cdk
 from resources.component import SharkordStack
 
 
+ACCOUNT: str = os.getenv(key="AWS_ACCOUNT_ID")
 REGION: str = os.getenv(key="AWS_REGION", default="us-east-1")
 
 
 app: aws_cdk.App = aws_cdk.App()
-sharkord_stack: SharkordStack = SharkordStack(scope=app, id="SharkordStack")
+sharkord_stack: SharkordStack = SharkordStack(
+    scope=app,
+    id="SharkordStack",
+    env=aws_cdk.Environment(account=ACCOUNT, region=REGION)
+)
 aws_cdk.Tags.of(scope=sharkord_stack).add(key="application", value="sharkord")
 app.synth(force=True, validate_on_synthesis=True)
